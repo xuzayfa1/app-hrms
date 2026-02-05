8f1f77f6023ca1b1cdb849bc5c8414bd7160fe0d
umidjonSuvonqulov <suvonqulov17@gmail.com>
2026-02-05 20:14:04 +0500

    umidjon-commit2

---
+++ file/src/main/kotlin/uz/zero/file/FileApplication.kt
@@ -0,0 +1,17 @@
+package uz.zero.file
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+import org.springframework.cloud.openfeign.EnableFeignClients
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories
+
+@SpringBootApplication
+@EnableJpaAuditing
+@EnableJpaRepositories(repositoryBaseClass = BaseRepositoryImpl::class)
+@EnableFeignClients
+class FileApplication
+
+fun main(args: Array<String>) {
+    runApplication<FileApplication>(*args)
+}
